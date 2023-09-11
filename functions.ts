b8b4a9f1efaee522a87010fe1b96bf7f12e29a81
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-11 08:43:16 +0200

    modified the function.ts

--- functions.ts
+++ functions.ts
@@ -21,3 +21,43 @@ function returnThis (num: number): string {
  * a function that does not return anything, will just have void as a return type by default.
  * we do not add it, typescript will infer it instead. hence it will know if the function does not return anything
  */
+
+// function as a Type:
+
+/**
+ * task: create a mexican wave:
+ * -> n this simple Kata your task is to create a function that turns a string into a Mexican Wave. 
+ * You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+ * 
+ * conditions:
+ * -> The input string will always be lower case but maybe empty
+ * ->  If the character in the string is whitespace then pass over it as if it was an empty seat
+ * 
+ * approach:
+ *  -> check if the string is !== "" if true: 
+ *  -> split the string into its spaces and turn it into an array
+ *  -> loop over the array
+ *  -> then count the length of each item inside the array
+ *  -> repeat the item with its length and store it in a new array
+ * -> loop over the ne array
+ * -> 
+ */
+
+const wave = (str:string): Array<string> => {
+    if(str !== "") {
+        const splitText = str.split(" ")
+        for(const txt of splitText) {
+            const txtRepeated = Array.from({length: 5}, ()=>{
+                return txt;
+            })
+            console.log(txtRepeated)
+            // for(const el of txtRepeated) {
+               
+            // }
+        }
+    } else {
+        console.log("")
+    }
+    return []
+}
+console.log(wave("hello"))
