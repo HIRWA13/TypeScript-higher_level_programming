8cc153208df83c070b36cfefa589fb4132603484
Junior Hirwa <iamhirwejr@gmail.com>
2023-09-14 08:59:47 +0200

    more on classes

--- 3classes.ts
+++ 3classes.ts
@@ -10,3 +10,37 @@ class Department {
 
 const accounting = new Department('Accounting');
 console.log(accounting)
+
+class Point {
+    x = 0;
+    y = 0;
+  }
+   
+  const pt = new Point();
+  // Prints 0, 0
+  console.log(`${pt.x}, ${pt.y}`);
+
+
+  class GoodGreeter {
+    name: string;
+   
+    constructor() {
+      this.name = "hello";
+    }
+  }
+
+  class Greeter {
+    readonly name: string = "world";
+   
+    constructor(otherName?: string) {
+      if (otherName !== undefined) {
+        this.name = otherName;
+      }
+    }
+   
+    err() {
+      this.name = "not ok";
+    }
+  }
+  const g = new Greeter();
+  g.name = "also not ok";
